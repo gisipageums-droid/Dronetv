6b6daa1d2cd7fa7ff509a7855abf5fe9d8d051d9
merndev6 <mern.dev6@ipageums.com>
2025-10-18 00:29:50 +0530

    Update Services.tsx

--- src/components/buildWeb/src/components/template/t2/final/edit/src/components/Services.tsx
+++ src/components/buildWeb/src/components/template/t2/final/edit/src/components/Services.tsx
@@ -375,7 +375,7 @@ export default function Services({serviceData, onStateChange, userId, publishedI
         <motion.div 
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className="fixed inset-0 bg-black/90 z-[999999] flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black/90 z-[99999999] flex items-center justify-center p-4"
         >
           <motion.div 
             initial={{ scale: 0.9, opacity: 0 }}
