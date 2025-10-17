fb3a223400b791e2f72fe31fdee1be8852cb526e
merndev6 <mern.dev6@ipageums.com>
2025-10-18 00:25:42 +0530

    Update Gallery.tsx

--- src/components/buildWeb/src/components/template/t2/final/edit/src/components/Gallery.tsx
+++ src/components/buildWeb/src/components/template/t2/final/edit/src/components/Gallery.tsx
@@ -360,7 +360,7 @@ const Gallery = ({galleryData,onStateChange, userId, publishedId, templateSelect
         <motion.div 
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className="fixed inset-0 bg-black/90 z-[999999] flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black/90 z-[99999999] flex items-center justify-center p-4"
         >
           <motion.div 
             initial={{ scale: 0.9, opacity: 0 }}
