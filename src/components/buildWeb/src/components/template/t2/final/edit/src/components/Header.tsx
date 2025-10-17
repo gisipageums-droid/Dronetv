7280593329d0a97a1386f4b9da00d492c29d5d00
merndev6 <mern.dev6@ipageums.com>
2025-10-18 00:27:05 +0530

    Update Header.tsx

--- src/components/buildWeb/src/components/template/t2/final/edit/src/components/Header.tsx
+++ src/components/buildWeb/src/components/template/t2/final/edit/src/components/Header.tsx
@@ -249,7 +249,7 @@ export default function Header({headerData,onStateChange,publishedId,userId,temp
         <motion.div 
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className="fixed inset-0 bg-black/90 z-[999999] flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black/90 z-[99999999] flex items-center justify-center p-4"
         >
           <motion.div 
             initial={{ scale: 0.9, opacity: 0 }}
