a34a1fddf2434da5150043ed68d3ad0d5b6766a8
merndev6 <mern.dev6@ipageums.com>
2025-10-21 21:42:43 +0530

    Update Profile.tsx

--- src/components/buildWeb/src/components/template/t1/edit/src/components/Profile.tsx
+++ src/components/buildWeb/src/components/template/t1/edit/src/components/Profile.tsx
@@ -790,7 +790,7 @@ export default function EditableCompanyProfile({
 
       {/* Crop Modal */}
       {cropModalOpen && (
-        <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-[100]">
+        <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-[9999999]">
           <div className="bg-white rounded-lg p-6 max-w-2xl w-full mx-4">
             <h3 className="text-xl font-bold mb-4 text-gray-900">Crop Company Image</h3>
             <div className="relative w-full h-96 bg-gray-900 rounded-lg overflow-hidden">
