364f8e6986840cd9811c2559a1ae2d9055f76499
merndev6 <mern.dev6@ipageums.com>
2025-10-28 22:35:23 +0530

    Update Blog.tsx

--- src/components/company/src/components/template/t2/final/edit/src/components/Blog.tsx
+++ src/components/company/src/components/template/t2/final/edit/src/components/Blog.tsx
@@ -322,7 +322,7 @@ export default function Blog({blogData, onStateChange , userId, publishedId, tem
 
             {/* Controls */}
             <div className="p-2 sm:p-3 bg-gray-50 border-t border-gray-200">
-              <div className="grid grid-cols-2 gap-2">
+              <div className="grid grid-cols-1 gap-2">
                 {/* Zoom Control */}
                 <div className="space-y-1.5">
                   <div className="flex items-center justify-between text-sm">
@@ -344,7 +344,7 @@ export default function Blog({blogData, onStateChange , userId, publishedId, tem
                 </div>
 
                 {/* Rotation Control */}
-                <div className="space-y-1.5">
+                {/* <div className="space-y-1.5">
                   <div className="flex items-center justify-between text-sm">
                     <span className="flex items-center gap-2 text-gray-700">
                       <RotateCw className="w-4 h-4" />
@@ -361,7 +361,7 @@ export default function Blog({blogData, onStateChange , userId, publishedId, tem
                     onChange={(e) => setRotation(Number(e.target.value))}
                     className="w-full h-1.5 bg-gray-300 rounded-lg appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-3.5 [&::-webkit-slider-thumb]:w-3.5 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-blue-500"
                   />
-                </div>
+                </div> */}
               </div>
 
               {/* Action Buttons - Equal width & responsive */}
@@ -503,7 +503,7 @@ export default function Blog({blogData, onStateChange , userId, publishedId, tem
                       <input
                         type="file"
                         accept="image/*"
-                        className="text-xs w-full cursor-pointer"
+                        className="text-xs font-bold w-full cursor-pointer"
                         onChange={(e) => handleBlogImageSelect(index, e)}
                       />
                       {pendingImages[index] && (
