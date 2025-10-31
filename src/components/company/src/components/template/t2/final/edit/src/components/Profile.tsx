9e13eb4943a489ea3374383a81464795a54d8042
merndev2 <mern.dev2@ipageums.com>
2025-10-31 17:49:15 +0530

    miner

--- src/components/company/src/components/template/t2/final/edit/src/components/Profile.tsx
+++ src/components/company/src/components/template/t2/final/edit/src/components/Profile.tsx
@@ -707,23 +707,23 @@ const Profile = ({
                     ) : (
                       <>
                         <a
-  href={member.socialLinks.twitter}
-  target="_blank"
-  className={`p-2 rounded-full ${
-    theme === "dark"
-      ? "bg-gray-800 hover:bg-gray-500"
-      : "bg-gray-100 hover:bg-gray-200"
-  }`}
-  aria-label="X (formerly Twitter)"
->
-  <svg
-    className="w-4 h-4"
-    fill="currentColor"
-    viewBox="0 0 24 24"
-  >
-    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-  </svg>
-</a>
+                          href={member.socialLinks.twitter}
+                          target="_blank"
+                          className={`p-2 rounded-full ${
+                            theme === "dark"
+                              ? "bg-gray-800 hover:bg-gray-500"
+                              : "bg-gray-100 hover:bg-gray-200"
+                          }`}
+                          aria-label="X (formerly Twitter)"
+                        >
+                          <svg
+                            className="w-4 h-4"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                          </svg>
+                        </a>
                         <a
                           href={member.socialLinks.linkedin}
                           target="_blank"
